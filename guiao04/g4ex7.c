68491c583822868a98649c02e21bffe4bed900a1
mestrebatista <jose.mestrebatista@gmail.com>
2021-04-20 16:03:25 +0100

    update

--- guiao04/g4ex7.c
+++ guiao04/g4ex7.c
@@ -12,9 +12,9 @@ void delay(int ms)
 int main(void)
 {
   unsigned char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F,\
-                      0x66, 0x6D, 0x7D, 0x07,\
-                      0x7F, 0x6F, 0x77, 0x7C,\
-                      0x39, 0x5E, 0x79, 0x71};
+                                    0x66, 0x6D, 0x7D, 0x07,\
+                                    0x7F, 0x6F, 0x77, 0x7C,\
+                                    0x39, 0x5E, 0x79, 0x71};
   unsigned char counter = 0;
   LATDbits.LATD6 = 1;                 // display high active
   LATDbits.LATD5 = 0;                 // display low inactive
